ecd071add25ea0815a4b39b222bed20732938361
Ni Chia <nichia@gmail.com>
2017-11-11 20:17:20 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,15 +3,15 @@ class Artist
   @@all = []
 
   def save
-    # where do we put this data?
-    # how do we refer to this object?
     @@all << self
   end
+
+  
 end
 
-taylor = Artst.new
-taylor.name = "Taylor"
-taylor.save #=> we store the instance of the artist in memory.
+#taylor = Artst.new
+#taylor.name = "Taylor"
+#taylor.save #=> we store the instance of the artist in memory.
 
 
 # 1. Instantiation and Saving Objectives
@@ -19,5 +19,3 @@ taylor.save #=> we store the instance of the artist in memory.
 # 3. Class Methods
 # 4. Enumeration - looping to find a match
 # 5. Introduce a module to share this functionality with songs.
-
- 
