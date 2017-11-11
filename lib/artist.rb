d26a082048ae8b6ee47074eb0e65b3204a465a52
Ni Chia <nichia@gmail.com>
2017-11-11 20:14:09 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,2 +1,23 @@
 class Artist
+  attr_accessor :name #=> What methods were added?
+  @@all = []
+
+  def save
+    # where do we put this data?
+    # how do we refer to this object?
+    @@all << self
+  end
 end
+
+taylor = Artst.new
+taylor.name = "Taylor"
+taylor.save #=> we store the instance of the artist in memory.
+
+
+# 1. Instantiation and Saving Objectives
+# 2. Class Variables
+# 3. Class Methods
+# 4. Enumeration - looping to find a match
+# 5. Introduce a module to share this functionality with songs.
+
+ 
